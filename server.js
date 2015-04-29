3d51a7cd7ea0db105dc09ba809d7f52ac32fe2ff
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-04-29 13:32:24 -0700

    finished basic server

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ var http = require('http'),
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		var postData = '';
 		var pathname = url.parse(request.url).pathname;
 		console.log('Request for ' + pathname + ' received.');
+
+		request.setEncoding('utf8');
+
+		request.addListener('data', function(postDataChunk) {
+			postData += postDataChunk;
+			console.log('Recieved POST data chunk "' + postDataChunk + '".');
+		});
+
+		request.addListener('end', function() {
+			route(handle, pathname, response, postData);
+
+		});
 	
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		var content = route(handle, pathname);
-		response.write(content);
-		response.end();
 	}
 
 	http.createServer(onRequest).listen(8888);
